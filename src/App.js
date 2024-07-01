41a2aab60f83ef8bdd37b1b0600b5cdc6d0dc1b8
HanLander <157143716+HanLander@users.noreply.github.com>
2024-07-01 17:47:16 +0100

    Built the basic search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello World
+      <div className="container">
+        <header className="App-header">Dictionary </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Hannah Lander and open sourced on Github</small>
+        </footer>
+      </div>
     </div>
   );
 }
