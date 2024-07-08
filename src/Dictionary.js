7ea04027f0b2dee6dcfd578ab511937ccf5e86ef
HanLander <157143716+HanLander@users.noreply.github.com>
2024-07-08 20:21:49 +0100

    Added the synonym function

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,7 +8,6 @@ export default function Dictionary() {
   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
   }
 
@@ -16,8 +15,7 @@ export default function Dictionary() {
     event.preventDefault();
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    console.log(apiUrl);
-    axios.get(apiUrl).then(handleResponse);
+    https: axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordhange(event) {
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,6 +1,8 @@
 import React from "react";
+import Synonyms from "./Synonyms.js";
 
 export default function Meaning(props) {
+  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3> {props.meaning.partOfSpeech}</h3>
@@ -8,8 +10,13 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
-              {definition.defintion}
-              <em> {definition.example} </em>
+              <strong>Definition: </strong>
+              {definition.definition}
+              <br />
+              <strong>Example: </strong>
+              <em>{definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
