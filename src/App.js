ae6a51aedaf6b7bb235c7b5efad0f3ba7b71207b
HanLander <157143716+HanLander@users.noreply.github.com>
2024-07-11 12:50:19 +0100

    Styled the application

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className="container">
         <header className="App-header">Dictionary </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="home" />
         </main>
         <footer className="App-footer">
           <small>Coded by Hannah Lander and open sourced on Github</small>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,31 +3,54 @@ import axios from "axios";
 import Results from "./Results.js";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordhange(event) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordhange} />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <h2>Search for a word</h2>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="defaultKeyword"
+              type="search"
+              onChange={handleKeywordhange}
+              defaultValue={props.defaultKeyword}
+            />
+          </form>
+          <div className="hint">E.g. React, Wine, Cinematography</div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
